1fef5322424a84360daecdb9ad8aab325fc2ffb7
jensepas <80039639+jensepas@users.noreply.github.com>
2025-04-15 16:47:03 +0200

    Added package.json and package-lock.json files

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+export default [
+    {
+        files: ["assets/**/*.js"], // Spécifiez vos fichiers JavaScript
+        rules: {
+            // Ajoutez vos règles personnalisées ici si nécessaire
+            "no-unused-vars": "warn",
+            "no-console": "off"
+        }
+    }
+];
